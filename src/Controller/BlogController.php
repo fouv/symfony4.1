3665cfd62ad66af8e9d2440200353e5ad4f5b72b
Florence Bouvier <florencebres1@gmail.com>
2018-11-11 19:29:03 +0100

    request bdd

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,35 +4,45 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Article;
 
 class BlogController extends AbstractController
 {
-   /**
-    *@Route("/blog", name="blog")
+  /**
+  * @Route("/blog", name="blog")
   */
-    public function index()
-    {
-        return $this->render('blog/index.html.twig', [
-            'controller_name' => 'BlogController',
-        ]);
-    }
+  public function index()
+  {
+    $repo = $this->getDoctrine()->getRepository(Article::class);
+    $articles = $repo->findAll();
 
-    /**
-    *@Route("/", name="home")
-    */
-    public function home()
-    {
-      return $this->render('blog/home.html.twig', [
-        'title' => "Bienvenue dans mon blog",
-        'age' => 31
-      ]);
-      }
+    return $this->render('blog/index.html.twig', [
+      'controller_name' => 'BlogController',
+      'articles' => $articles
+    ]);
+  }
 
-      /**
-      *@Route("/blog/12", name="blog_show")
-      */
-      public function show()
-      {
-        return $this->render('blog/show.html.twig');
-      }
+  /**
+  * @Route("/", name="home")
+  */
+  public function home()
+  {
+    return $this->render('blog/home.html.twig', [
+      'title' => "Bienvenue dans mon blog",
+      'age' => 31
+    ]);
+  }
+
+  /**
+  * @Route("/blog/{$id}", name="blog_show")
+  */
+  public function show($id){
+
+    $repo = $this->getDoctrine()->getRepository(Article::class);
+
+    $article = $repo->find($id);
+    return $this->render('blog/show.html.twig', [
+      'article' => $article
+    ]);
+  }
 }
